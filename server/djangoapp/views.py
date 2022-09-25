078a493e3a271dda55f2f1626894326f6b2e1118
ParthPatel <parthpatel199618@gmail.com>
2022-09-24 22:33:40 -0400

    Week 2 User Management

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -41,6 +41,7 @@ def login_request(request):
         if user is not None:
             login(request, user)
             return redirect('djangoapp:index')
+            # return render(request, 'djangoapp/index.html', context)
         else:
             return redirect('djangoapp:index')
     else:
@@ -52,8 +53,28 @@ def logout_request(request):
     return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        password = request.POST['password']
+
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.debug('{} is new User'.format(username))
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password)
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
