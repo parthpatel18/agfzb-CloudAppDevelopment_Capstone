26365db097a939123b694556bac3ae5f1b00b1e4
Parth Patel <parthpatel199618@gmail.com>
2022-11-12 03:28:04 -0500

    Week 3 CarModel and CarMake

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,14 +4,33 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# <HINT> Create a Car Make model
+class CarMake(models.Model):
+    name = models.CharField('Name', max_length=30)
+    description = models.CharField('Description', max_length=250)
 
+    def __str__(self):
+        return self.name
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# <HINT> Create a Car Model model 
+class CarModel(models.Model):
+    car_type_choices = (
+        ('HT', 'Hatchback'),
+        ('PU', 'Pickup'),
+        ('SD', 'Sedan'),
+        ('SU', 'SUV'),
+        ('WA', 'Wagon'),
+    )
+
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField('Dealer ID', default=0)
+    name = models.CharField('Name', max_length=30)
+    car_type = models.CharField('Type', max_length=2, choices=car_type_choices)
+    year = models.DateField('Year')
+
+    def __str__(self):
+        return str(self.car_make) + ' ' + self.name
+
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
 # - Dealer id, used to refer a dealer created in cloudant database
