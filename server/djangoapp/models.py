967554147297dd533a07df113d01cbf24096415f
Parth Patel <parthpatel199618@gmail.com>
2022-11-14 23:50:13 -0500

    Week3 Lab2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -31,16 +31,20 @@ class CarModel(models.Model):
     def __str__(self):
         return str(self.car_make) + ' ' + self.name
 
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
-
-
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        self.address = address
+        self.city = city
+        self.full_name = full_name
+        self.id = id
+        self.lat = lat
+        self.long = long
+        self.short_name = short_name
+        self.st = st
+        self.zip = zip
 
+        def __str__(self):
+            return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
